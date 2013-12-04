f96f83a5d7e8b4e055f0a5d37c0c376fd4e7242a
smallcode <14190635@qq.com>
2013-12-04 11:54:24 +0800

    添加ApiError

---
+++ chinaapi/utils/exceptions.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+class ApiError(StandardError):
+    def __init__(self, error_code, error, request):
+        self.error_code = error_code
+        self.error = error
+        self.request = request
+        StandardError.__init__(self, error)
+
+    def __str__(self):
+        return 'APIError: %s: %s, request: %s' % (self.error_code, self.error, self.request)
