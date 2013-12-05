7e8899297ee08971cae210c5ab64400f3d2e4268
smallcode <14190635@qq.com>
2013-12-06 00:20:48 +0800

    增加人人API测试

--- tests/test_renren.py
+++ tests/test_renren.py
@@ -7,6 +7,9 @@ from chinaapi.utils.models import App, Token
 
 class RenRenTest(TestCase):
     def setUp(self):
+        """
+        测试时需填写app_key,app_secret,access_token
+        """
         app = App('app_key', 'app_secret')  # 填上自己的app_key，app_secret
         token = Token('access_token')  # 填上取得的access_token
         self.client = ApiClient(app)
