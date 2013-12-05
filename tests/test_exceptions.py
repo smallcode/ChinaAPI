45155450e40c2ad6e357dc4da69c7e52bfa49f54
smallcode <14190635@qq.com>
2013-12-05 21:31:59 +0800

    增加异常类的测试

---
+++ tests/test_exceptions.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from unittest import TestCase
+from chinaapi.utils.exceptions import ApiError
+
+
+class ExceptionTest(TestCase):
+    def test_api_error(self):
+        error = ApiError('http://request_url', 404, 'Request Api not found!')
+        self.assertEqual('[404]: Request Api not found!, request: http://request_url', str(error))
+
+    def test_api_error_with_sub(self):
+        error = ApiError('http://request_url', 404, 'Request Api not found!', 1000, 'sub error msg')
+        self.assertEqual('[404]: Request Api not found!, [1000]: sub error msg, request: http://request_url',
+                         str(error))
