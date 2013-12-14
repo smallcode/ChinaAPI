10498ddd64067c8e409afe5f7866bd6d2e3fc0f0
smallcode <14190635@qq.com>
2013-12-14 23:54:55 +0800

    add web api

---
+++ chinaapi/utils/web.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+from chinaapi.utils.api import Request
+
+
+class ClientBase(Request):
+    def __init__(self):
+        super(ClientBase, self).__init__()
+        self._session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36'
