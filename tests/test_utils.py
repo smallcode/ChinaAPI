8ae1deaab2562190602f845705e47c3e9c1af60d
smallcode <45945756@qq.com>
2014-06-20 10:20:44 +0800

    修改单元测试：test_parse_querystring

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ from chinaapi.utils import parse_querystring
 
 class UtilsTest(TestCase):
     def test_parse_querystring(self):
-        r = parse_querystring("a=a&b=b")
+        r = parse_querystring("http://test.com?foo=bar&n=1")
         self.assertEqual(2, len(r))
-        self.assertEqual('a', r['a'])
-        self.assertEqual('b', r['b'])
+        self.assertEqual(r['foo'], 'bar')
+        self.assertEqual( r['n'], '1')
